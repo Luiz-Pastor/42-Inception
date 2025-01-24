44148f83bc07966efa458a3ff3237305dce434e9
Luiz-Pastor <luigpacr5@gmail.com>
2025-01-24 23:11:05 +0100

    Added perms to the folders

--- srcs/requirements/mariadb/tools/configure_mariadb.sh
+++ srcs/requirements/mariadb/tools/configure_mariadb.sh
@@ -3,7 +3,14 @@
 # Create the folder to the execution temporal files
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
+chmod -R 755 /var/run/mysqld
 
 # Create the folder where the data will be saved
 mkdir -p /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
+chmod -R 750 /var/lib/mysql
+
+# Create the logs folder
+mkdir -p /var/log/mysql
+chown -R mysql:mysql /var/log/mysql
+chmod -R 750 /var/log/mysql
